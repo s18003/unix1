f71eb6680e2e8940f896bbbe5f0d8752d502437b
s18003 <s18003@std.it-college.ac.jp>
2018-06-06 04:39:34 +0000

    06/06の課題　

---
+++ 0606/060601.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/vagrant
+_IFS=$IFS
+IFS=$'\n'
+
+for file in $(find . -type f)
+do
+	size=$(ls -l $file | awk '{print $5}')
+	if [ $size -eq 0 ]; then
+		echo "$file"
+	fi
+done 
+
+IFS=$_IFS
